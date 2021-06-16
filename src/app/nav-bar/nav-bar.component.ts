a64246d4fa7bf9b8204fddd5ba7257224c29df29
Kamil Lihan <k.lihan@outlook.com>
2021-06-16 19:58:00 +0200

    implemented scrolling

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -14,18 +14,28 @@ export class NavBarComponent implements OnInit {
   }
 
   public goHome() {
+    this.router.navigate(['/']);
     document.getElementById('home').scrollIntoView({behavior: 'smooth'});
   }
 
   public goAbout() {
+    this.router.navigate(['/about']);
     document.getElementById('about').scrollIntoView({behavior: 'smooth'});
   }
 
   public goDev() {
+    this.router.navigate(['/dev']);
     document.getElementById('dev').scrollIntoView({behavior: 'smooth'});
   }
 
   public goExp() {
+    this.router.navigate(['/exp']);
     document.getElementById('exp').scrollIntoView({behavior: 'smooth'});
+  }
+
+  public goContact() {
+    this.router.navigate(['/contact']);
+    document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+  
   }
 }
