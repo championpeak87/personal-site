69f9ca6abbd772cb190caef7e91530f7d95d7065
Kamil Lihan <k.lihan@outlook.com>
2021-07-08 17:16:08 +0200

    dev stack

--- src/app/sections/dev-section/dev-section.component.ts
+++ src/app/sections/dev-section/dev-section.component.ts
@@ -7,18 +7,56 @@ import { ItemcardWideItem } from 'src/app/components/itemcard-wide/itemcard-wide
   styleUrls: ['./dev-section.component.scss']
 })
 export class DevSectionComponent implements OnInit {
-  public devstackItems: ItemcardWideItem[] = [{
+  public devstackWebItems: ItemcardWideItem[] = [{
     img: '../../../assets/angular.svg',
     header: 'Angular',
     description: 'A single page web framework',
     percentage: 60
   },
   {
-    img: '../../../assets/postgresql.svg',
-    header: 'PostgreSQL',
-    description: 'A relational database software',
+    img: '../../../assets/react.svg',
+    header: 'React',
+    description: 'A web development library',
+    percentage: 20
+  },
+  {
+    img: '../../../assets/nodejs.svg',
+    header: 'Node.JS',
+    description: 'A JavaScript runtime',
     percentage: 50
   }];
+
+  public devstackMobileItems: ItemcardWideItem[] = [{
+    img: '../../../assets/android.svg',
+    header: 'Android framework',
+    description: 'Development of Android mobile apps',
+    percentage: 70
+  }];
+
+  public devstackOtherItems: ItemcardWideItem[] = [{
+    img: '../../../assets/git.svg',
+    header: 'GIT',
+    description: 'A version control system',
+    percentage: 70
+  },
+{
+  img: '../../../assets/docker.svg',
+  header: 'Docker',
+  description: 'A containerization tool',
+  percentage: 70
+},
+{
+  img: '../../../assets/jenkins.svg',
+  header: 'Jenkins',
+  description: 'A tool for automated building and deployment',
+  percentage: 20
+}, 
+{
+  img: '../../../assets/spring.svg',
+  header: 'Spring framework',
+  description: 'A backend framework',
+  percentage: 30
+}];
 
   constructor() { }
 
